f593f12120e8df53266b23447d6814c025b26c16
EXtremeExploit <gdextremeexploit@gmail.com>
2020-07-07 20:24:22 -0300

    debug var, better logging and improve code quality

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-require(`dotenv`).config();
-const banchojs = require(`bancho.js`);
+require('dotenv').config();
+const banchojs = require('bancho.js');
+const debug = process.env.debug;
 var client = new banchojs.BanchoClient({
 	username: process.env.osuUsername,
 	password: process.env.osuPassword
 });
 
-async function Update() {
-	if (!client.isConnected())
-		await client.connect();
-	await client.getUser(`Tillerino`).sendMessage(`!u`);
-	return null;
-}
+var updateClient = 'Ameo';
 
-client.on("PM", (msg)=>{
-	console.log(`[${new Date}] ${msg.user.ircUsername} > ${msg.recipient.ircUsername}: ${msg.content}`);
-});
-
-client.on('state',(s,e) => {
-	if(s == banchojs.ConnectStates.Connected){
+client.on('state', (s, e) => {
+	if (s == banchojs.ConnectStates.Connected) {
 		console.log(`[${new Date}] Connected to ${process.env.osuUsername}`);
 	}
 })
+
+
+async function Update() {
+	if (!client.isConnected())
+		await client.connect();
+	await client.getUser(updateClient).sendMessage('!u');
+	return null;
+}
+
+client.on("PM", (msg) => {
+	// if (msg.recipient.ircUsername == updateClient || msg.user.ircUsername == updateClient)
+	// 	console.log(`[${new Date}] ${msg.user.ircUsername} > ${msg.recipient.ircUsername}: ${msg.content}`);
+
+	if(msg.content == '!u' && msg.recipient.ircUsername == updateClient){
+		console.log(`[${new Date}] Sent !u to ${updateClient}`)
+	}
+
+	if(msg.user.ircUsername == updateClient){
+		console.log(msg.content)
+	}
+});
 
 let d;
 let limited = false;
@@ -29,12 +42,14 @@ setInterval(() => {
 	if (d.getMinutes() == 0 && d.getSeconds() == 0 && limited == false) {
 		Update().then(() => {
 			limited = true;
-			console.info(`[${new Date}] limited = true`)
+			if (debug)
+				console.log(`[${new Date}] limited = true`)
 		});
 	} else {
 		if (limited == true) {
 			limited = false;
-			console.info(`[${new Date}] limited = false`)
+			if (debug)
+				console.log(`[${new Date}] limited = false`)
 		}
 	}
 }, 500);
